a22f0b0f70cf6291aea67a6ea54ed05a4f914b69
hour Aldahrouj <mohammad.a.j.1998@gmail.com>
2021-03-06 04:42:11 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $(function () {
           navH = $('.navbar').innerHeight();
      $('.one-time').innerHeight(winH-(ipperH + navH));
     });
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1){  
+          $('header').addClass("sticky");
+        }
+        else{
+          $('header').removeClass("sticky");
+        }
+      });
