f2dbf2e715692f6e2956d94ff240ca27656d01ba
hour Aldahrouj <mohammad.a.j.1998@gmail.com>
2021-03-13 07:00:51 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-$(function () {
-    'use strict';
-    //adjustslider height
-     var winH = $(window).height(),
-        ipperH = $('.upper-bar').innerHeight(),  
-          navH = $('.navbar').innerHeight();
-     $('.one-time').innerHeight(winH-(ipperH + navH));
-    });
+
 
     $(window).scroll(function() {
       if ($(this).scrollTop() > 1){  
